7695016de38a4667ed295ba17df57d59fa9c0b15
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2021-06-08 16:18:08 +1200

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 function Header() {
     return (
       <header className="header">
-        header
+        <div className="header-overlay">
+          <div className="search-wrapper">
+            <h1 className="logo">Photolocations</h1>
+            <div className="search">
+              <label className="sr-only" for="search">Search</label>
+              <input className="search-field" type="text" id="search" name="search" />
+            </div>
+          </div>
+        </div>
       </header>
     )
 }
